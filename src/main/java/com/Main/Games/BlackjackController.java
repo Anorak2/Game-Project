9afb8d9ef19429c6ae749eb37582be8d92780fea
3064683 <adamcoleberry@gmail.com>
2021-11-09 08:45:10 -0600

    Added Blackjack Cards

--- src/main/java/com/Main/Games/BlackjackController.java
+++ src/main/java/com/Main/Games/BlackjackController.java
@@ -1,9 +1,9 @@
 package com.Main.Games;
 
 import com.Main.MenuController;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
@@ -33,23 +33,81 @@ public class BlackjackController extends MenuController {
         }
     }
 
+
     @FXML
     Button DrawButton;
     @FXML
     Button StandButton;
     @FXML
-    Text PlayerHandText;
-    @FXML
-    Text DealerHandText;
-    @FXML
     Text WinnerText;
+    @FXML
+    Rectangle PlayerCard1;
+    @FXML
+    Rectangle PlayerCard2;
+    @FXML
+    Rectangle PlayerCard3;
+    @FXML
+    Rectangle PlayerCard4;
+    @FXML
+    Rectangle PlayerCard5;
+    @FXML
+    Rectangle PlayerCard6;
+    @FXML
+    Rectangle PlayerCard7;
+    @FXML
+    Text PlayerNum1;
+    @FXML
+    Text PlayerNum2;
+    @FXML
+    Text PlayerNum3;
+    @FXML
+    Text PlayerNum4;
+    @FXML
+    Text PlayerNum5;
+    @FXML
+    Text PlayerNum6;
+    @FXML
+    Text PlayerNum7;
+    @FXML
+    Rectangle DealerCard1;
+    @FXML
+    Rectangle DealerCard2;
+    @FXML
+    Rectangle DealerCard3;
+    @FXML
+    Rectangle DealerCard4;
+    @FXML
+    Rectangle DealerCard5;
+    @FXML
+    Rectangle DealerCard6;
+    @FXML
+    Rectangle DealerCard7;
+    @FXML
+    Text DealerText1;
+    @FXML
+    Text DealerText2;
+    @FXML
+    Text DealerText3;
+    @FXML
+    Text DealerText4;
+    @FXML
+    Text DealerText5;
+    @FXML
+    Text DealerText6;
+    @FXML
+    Text DealerText7;
     ArrayList<Card> deck = new ArrayList<Card>();
     ArrayList<Card> player = new ArrayList<Card>();
     ArrayList<Card> dealer = new ArrayList<Card>();
     Boolean Stand = false;
 
+
     public void initialize() {
         //create deck
+        deck.clear();
+        player.clear();
+        dealer.clear();
+        hideAllCards();
         for (int x = 2; x < 15; x++)
             deck.add(new Card("Spade", x));
         for (int x = 2; x < 15; x++)
@@ -58,24 +116,24 @@ public class BlackjackController extends MenuController {
             deck.add(new Card("Heart", x));
         for (int x = 2; x < 15; x++)
             deck.add(new Card("Diamond", x));
-        PlayerHandText.setText("");
-        DealerHandText.setText("");
         DrawDealer();
         WinnerText.setText("");
+        DrawPlayer();
+        DrawPlayer();
     }
 
-    public void DrawPlayer(ActionEvent e){
+    public void DrawPlayer(){
         if(!Stand){
             Card temp = drawRandomCard();
             player.add(temp);
-            displayPlayerCard(temp);
+            displayPlayerCard();
         }
         if(getTotal(player) > 21)
             dealerWins();
         else if (getTotal(player) == 21)
             playerWins();
     }
-    public void StandPlayer(ActionEvent e){
+    public void StandPlayer(){
         Stand = true;
         DrawButton.setVisible(false);
         StandButton.setVisible(false);
@@ -96,61 +154,104 @@ public class BlackjackController extends MenuController {
         StandButton.setVisible(false);
         WinnerText.setText("You Win!");
     }
-    public void displayPlayerCard(Card card){
-        String temp = "";
-        if(card.getSuite().equals("Spade"))
-            temp = " of Spades";
-        else if(card.getSuite().equals("Club"))
-            temp = " of Clubs";
-        else if(card.getSuite().equals("Heart"))
-            temp = " of Hearts";
-        else if(card.getSuite().equals("Diamond"))
-            temp = " of Diamonds";
-        if(card.getNum() == 14)
-            PlayerHandText.setText(PlayerHandText.getText() + "Ace" + temp + ", ");
-        else if(card.getNum() == 13)
-            PlayerHandText.setText(PlayerHandText.getText() + "King" + temp + ", ");
-        else if(card.getNum() == 12)
-            PlayerHandText.setText(PlayerHandText.getText() + "Queen" + temp + ", ");
-        else if(card.getNum() == 11)
-            PlayerHandText.setText(PlayerHandText.getText() + "Jack" + temp + ", ");
-        else
-            PlayerHandText.setText(PlayerHandText.getText() + card.getNum() + temp + ", ");
-
+    public void displayPlayerCard(){
+        if(player.size() >= 1) {
+            PlayerCard1.setVisible(true);
+            PlayerNum1.setVisible(true);
+            PlayerNum1.setText(returnNum(player.get(0)));
+        }
+        if(player.size() >= 2) {
+            PlayerCard2.setVisible(true);
+            PlayerNum2.setVisible(true);
+            PlayerNum2.setText(returnNum(player.get(1)));
+        }
+        if(player.size() >= 3) {
+            PlayerCard3.setVisible(true);
+            PlayerNum3.setVisible(true);
+            PlayerNum3.setText(returnNum(player.get(2)));
+        }
+        if(player.size() >= 4) {
+            PlayerCard4.setVisible(true);
+            PlayerNum4.setVisible(true);
+            PlayerNum4.setText(returnNum(player.get(3)));
+        }
+        if(player.size() >= 5) {
+            PlayerCard5.setVisible(true);
+            PlayerNum5.setVisible(true);
+            PlayerNum5.setText(returnNum(player.get(4)));
+        }
+        if(player.size() >= 6) {
+            PlayerCard6.setVisible(true);
+            PlayerNum6.setVisible(true);
+            PlayerNum6.setText(returnNum(player.get(5)));
+        }
+        if(player.size() >= 7) {
+            PlayerCard7.setVisible(true);
+            PlayerNum7.setVisible(true);
+            PlayerNum7.setText(returnNum(player.get(6)));
+        }
     }
 
     public void DrawDealer(){
         Card temp = drawRandomCard();
         dealer.add(temp);
-        displayDealerCard(temp);
+        displayDealerCard();
     }
     public void dealerWins(){
         DrawButton.setVisible(false);
         StandButton.setVisible(false);
         WinnerText.setText("You Lose");
     }
-    public void displayDealerCard(Card card){
-        String temp = "";
-        if(card.getSuite().equals("Spade"))
-            temp = " of Spades";
-        if(card.getSuite().equals("Club"))
-            temp = " of Clubs";
-        if(card.getSuite().equals("Heart"))
-            temp = " of Hearts";
-        if(card.getSuite().equals("Diamond"))
-            temp = " of Diamonds";
-        if(card.getNum() == 14)
-            DealerHandText.setText(DealerHandText.getText() + "Ace" + temp + ", ");
-        else if(card.getNum() == 13)
-            DealerHandText.setText(DealerHandText.getText() + "King" + temp + ", ");
-        else if(card.getNum() == 12)
-            DealerHandText.setText(DealerHandText.getText() + "Queen" + temp + ", ");
-        else if(card.getNum() == 11)
-            DealerHandText.setText(DealerHandText.getText() + "Jack" + temp + ", ");
-        else
-            DealerHandText.setText(DealerHandText.getText() + card.getNum() + temp + ", ");
+    public void displayDealerCard(){
+        if(dealer.size() >= 1) {
+            DealerCard1.setVisible(true);
+            DealerText1.setVisible(true);
+            DealerText1.setText(returnNum(dealer.get(0)));
+        }
+        if(dealer.size() >= 2) {
+            DealerCard2.setVisible(true);
+            DealerText2.setVisible(true);
+            DealerText2.setText(returnNum(dealer.get(1)));
+        }
+        if(dealer.size() >= 3) {
+            DealerCard3.setVisible(true);
+            DealerText3.setVisible(true);
+            DealerText3.setText(returnNum(dealer.get(2)));
+        }
+        if(dealer.size() >= 4) {
+            DealerCard4.setVisible(true);
+            DealerText4.setVisible(true);
+            DealerText4.setText(returnNum(dealer.get(3)));
+        }
+        if(dealer.size() >= 5) {
+            DealerCard5.setVisible(true);
+            DealerText5.setVisible(true);
+            DealerText5.setText(returnNum(dealer.get(4)));
+        }
+        if(dealer.size() >= 6) {
+            DealerCard6.setVisible(true);
+            DealerText6.setVisible(true);
+            DealerText6.setText(returnNum(dealer.get(5)));
+        }
+        if(dealer.size() >= 7) {
+            DealerCard7.setVisible(true);
+            DealerText7.setVisible(true);
+            DealerText7.setText(returnNum(dealer.get(6)));
+        }
     }
 
+    public String returnNum(Card card){
+        if(card.getNum() == 14)
+            return "A";
+        else if(card.getNum() == 13)
+            return "K";
+        else if(card.getNum() == 12)
+            return "Q";
+        else if(card.getNum() == 11)
+            return "J";
+        else
+           return "" + card.getNum();
+    }
     public Card drawRandomCard(){
         int x  = (int) (Math.random() *deck.size());
         Card temp = deck.get(x);
@@ -176,6 +277,39 @@ public class BlackjackController extends MenuController {
             }
         }
         return total;
+    }
+    public void hideAllCards(){
+        PlayerCard1.setVisible(false);
+        PlayerCard2.setVisible(false);
+        PlayerCard3.setVisible(false);
+        PlayerCard4.setVisible(false);
+        PlayerCard5.setVisible(false);
+        PlayerCard6.setVisible(false);
+        PlayerCard7.setVisible(false);
+
+        PlayerNum1.setVisible(false);
+        PlayerNum2.setVisible(false);
+        PlayerNum3.setVisible(false);
+        PlayerNum4.setVisible(false);
+        PlayerNum5.setVisible(false);
+        PlayerNum6.setVisible(false);
+        PlayerNum7.setVisible(false);
+
+        DealerCard1.setVisible(false);
+        DealerCard2.setVisible(false);
+        DealerCard3.setVisible(false);
+        DealerCard4.setVisible(false);
+        DealerCard5.setVisible(false);
+        DealerCard6.setVisible(false);
+        DealerCard7.setVisible(false);
+
+        DealerText1.setVisible(false);
+        DealerText2.setVisible(false);
+        DealerText3.setVisible(false);
+        DealerText4.setVisible(false);
+        DealerText5.setVisible(false);
+        DealerText6.setVisible(false);
+        DealerText7.setVisible(false);
     }
 }
 
